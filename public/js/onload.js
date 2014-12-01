e02031d7e3d04475e39644eec7aef964ad8466e5
Giovanni Delacqua <giovannidelacqua@lm3451.local>
2014-12-01 16:31:40 -0600

    bungie api added

--- public/js/onload.js
+++ public/js/onload.js
@@ -1,4 +1,12 @@
 $('document').ready(function(){
 
+  var membershipType = 2,
+    membershipId = '4611686018428410896',
+    accountType = 'TigerPSN',
+    characterId = '2305843009215383036', //titan
+    url1 = 'http://www.bungie.net/Platform/Destiny/' + accountType + '/Account/' + membershipId + '/Character/' + characterId + '/Inventory/'
+    url = 'http://www.bungie.net/en/Legend/' + membershipType + '/' + membershipId + '/' + characterId;
 
+    console.log(url); // pretty bungie link
+    console.log(url1); //inventory
 });
